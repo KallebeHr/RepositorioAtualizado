21773460d795fabf061f3f9add48c431d1686841
KallebeHr <kallebealmeidap2@gmail.com>
2025-12-16 17:07:31 -0300

    NovoBanco

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu2YCg2ieaTek9aKcMgxqHBPB6L4yMEYU",
+  apiKey: "AIzaSyALP3q8oYZkAsCwVRXmsaa7VQjcxiUCQIg",
   authDomain: "repertorio-atualizado-2f151.firebaseapp.com",
-  projectId: "repertorio-atualizado-2f151",
-  storageBucket: "repertorio-atualizado-2f151.firebasestorage.app",
-  messagingSenderId: "1063978224223",
-  appId: "1:1063978224223:web:efa3c7232f47067e7c29ea",
-  measurementId: "G-1N2TSLKDRF"
+  projectId: "repertorio-d3552",
+  storageBucket: "repertorio-d3552.firebasestorage.app",
+  messagingSenderId: "129323465959",
+  appId: "1:129323465959:web:c47094b11b143bd8ac3642",
+  measurementId: "G-V650X867G5"
 };
 
 // Inicializa Firebase
